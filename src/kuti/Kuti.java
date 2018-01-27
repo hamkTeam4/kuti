a28b89f8ed47d819dd6d925a98f77d56605b0649
deeqkko <kimmo.koivusalo@student.hamk.fi>
2018-01-27 16:01:35 +0200

    Empty project

--- src/kuti/Kuti.java
+++ src/kuti/Kuti.java
@@ -15,7 +15,7 @@ public class Kuti {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic her
     }
     
 }
